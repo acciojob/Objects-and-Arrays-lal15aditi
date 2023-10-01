f6dfde1dc162c454a993fcd827ed4f30b29fdec0
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2023-10-01 18:26:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ const person = {
 };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+const team = players;
 
+// Create a copy of the players array
+const team1 = [...players];
+
+// Create a copy of the person object
+const cap1 = { ...person };
 window.players = players;
 window.person = person;
 window.team = team;
